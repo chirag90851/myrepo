1f1885204b7a9369e79baf967202a362432dee17
Administrator@ALPHA.LOCAL <Administrator@ALPHA.LOCAL>
2020-02-20 10:43:46 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from  '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule,MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+     HttpClientModule,
+      MatButtonModule,
+       MatMenuModule,
+        MatCardModule,
+         MatToolbarModule,
+          MatIconModule,
+           MatSidenavModule,
+            MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
